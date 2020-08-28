a9aa01faf3894cd68d39454f67ec461faf15b211
Jerry Robertson <jerry.r.robertson1@gmail.com>
2020-08-28 13:37:33 -0500

    challenge completed

--- scripts/Joke.js
+++ scripts/Joke.js
@@ -1,2 +1,11 @@
 // Define and export a function called Joke.
 // It should accept a joke object as a parameter and return an html string
+export const Joke = (input,helper) => {
+// return `<p class="joke-punchline">${input.setup}${input.punchline}</p>`
+    if (helper === "punchline") { 
+        // const contentPunchline = document.querySelector(".joke-punchline")
+        // contentPunchline.innerHTML += 
+        return `<p class="joke-punchline">${input.punchline}</p>` 
+    }
+    else { return `<p class="joke-setup">${input.setup}</p>` }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,32 @@
-const jokeBtn = document.querySelector(".request-joke-btn");
+import { useJoke, getJoke } from "./JokeProvider.js";
+import { Joke } from "./Joke.js";
 
+const jokeBtn = document.querySelector(".request-joke-btn");
 jokeBtn.addEventListener("click", (e) => {
+  const contentSetup = document.querySelector(".joke-setup")
+  contentSetup.innerHTML = `<button class="request-joke-btn2">Tell me the punchline!</button>`
   console.log("I should be grabbing a joke now...");
   // TODO: fetch a joke from the Joke API and render it to the DOM
-});
+  getJoke().then(_ => { 
+    const step1 = useJoke()
+    contentSetup.innerHTML += Joke(step1,"setup")
+  })
+  .then(_ => {
+    console.log("Then tester.");
+    const jokeBtn2 = document.querySelector(".request-joke-btn2");
+    jokeBtn2.addEventListener("click", (f) => {
+      console.log("I should be grabbing the punchline now...");
+      // TODO: fetch a joke from the Joke API and render it to the DOM
+      // getJoke().then(_ => { 
+        const contentPunchline = document.querySelector(".joke-punchline")
+        const step2 = useJoke()
+        console.log("Joked Used",step2)
+        contentPunchline.innerHTML = Joke(step2,"punchline")
+      // })
+    })
+  
+  })
+
+})
+
+
